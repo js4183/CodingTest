2f89c2cd4bf04552198157cd308fc3b75dc6e6c1
주상민 <112377313+js4183@users.noreply.github.com>
2023-01-28 22:46:43 +0900

    이진수더하기

---
+++ src/Level0/이진수더하기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        long newValue = Long.parseLong(bin1, 2) + Long.parseLong(bin2, 2);
+        return Long.toBinaryString(newValue);
+    }
+}
